76a9328be5674f2d710154cdc2d05e869c4e41aa
Ahmed Ibrahim <ib153507@gmail.com>
2022-08-31 10:28:51 +0200

    Add create table regex

--- src/regex.rs
+++ src/regex.rs
@@ -1,1 +1,2 @@
-pub const DB_REGEX: &str = r"(?im)(?P<action>[^\s;]+) database (?P<name>[^;]+)";
+pub const RE_DB: &str = r"(?im)(?P<action>[^\s;]+) database (?P<name>[^;]+)";
+pub const RE_CREATE_TABLE: &str = r"(?im)create table (?P<name>[^\(\s]+)(\s|)(?P<entries>[^;]+)";
