8bf0b39151243bcf801d8496c91cdb841048c208
Ahmed Ibrahim <ib153507@gmail.com>
2022-08-31 11:49:35 +0200

    Write suite case for drop & truncate tables

--- src/query_parser.rs
+++ src/query_parser.rs
@@ -83,14 +83,23 @@ impl QueryParser {
             return Ok(Query::Table(TableQuery::Create { table_name, cols }));
         }
 
+        let re_table = Regex::new(RE_TABLE).unwrap();
+
+        if let Some(caps) = re_table.captures(query) {
+            let table_name = caps["name"].to_string();
+            match caps["action"].to_lowercase().as_str() {
+                "drop" => return Ok(Query::Table(TableQuery::Drop(table_name))),
+                "truncate" => return Ok(Query::Table(TableQuery::Truncate(table_name))),
+                _ => {}
+            };
+        }
+
         Err("Invalid query.")
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use std::collections::HashMap;
-
     use crate::query_parser::{DatabaseAction, Query, TableQuery};
 
     use super::QueryParser;
@@ -151,6 +160,8 @@ mod tests {
             assert_eq!(cols.get("age").unwrap(), "int");
             assert_eq!(cols.get("name").unwrap(), "varchar");
             assert_eq!(cols.get("id").unwrap(), "int");
+        } else {
+            panic!("Unexpted query");
         }
     }
 
@@ -169,6 +180,28 @@ mod tests {
             assert_eq!(cols.get("column1").unwrap(), "datatype");
             assert_eq!(cols.get("column2").unwrap(), "datatype");
             assert_eq!(cols.get("column3").unwrap(), "datatype");
+        } else {
+            panic!("Unexpted query");
+        }
+    }
+
+    #[test]
+    fn drop_table() {
+        let query = QueryParser::parse(r#"DROP TABLE demo"#).unwrap();
+        if let Query::Table(TableQuery::Drop(t_name)) = query {
+            assert_eq!(t_name, "demo".to_string());
+        } else {
+            panic!("Unexpted query")
+        }
+    }
+
+    #[test]
+    fn truncate_table() {
+        let query = QueryParser::parse(r#"TRUNCATE TABLE demo"#).unwrap();
+        if let Query::Table(TableQuery::Truncate(t_name)) = query {
+            assert_eq!(t_name, "demo".to_string());
+        } else {
+            panic!("Unexpted query")
         }
     }
 }
