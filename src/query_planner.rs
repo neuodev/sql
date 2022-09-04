504d376342fe96fc67417caa04c6efe562aeb29a
Ahmed Ibrahim <ib153507@gmail.com>
2022-09-04 05:05:16 +0200

    Match on parsed query

--- src/query_planner.rs
+++ src/query_planner.rs
@@ -1,5 +1,10 @@
-use crate::query_parser::{QueryParser, QueryParserError};
-use inquire::{Editor, InquireError};
+use std::ffi::OsStr;
+
+use crate::query_parser::{DatabaseAction, Query, QueryParser, QueryParserError, TableQuery};
+use inquire::{
+    validator::{StringValidator, Validation},
+    Editor, InquireError,
+};
 use thiserror::Error;
 
 #[derive(Debug, Error)]
@@ -13,18 +18,53 @@ pub enum QueryPlannerError {
 pub struct QueryPlanner;
 impl QueryPlanner {
     pub fn new() -> Result<(), QueryPlannerError> {
-        let query = Editor::new("SQL query")
-            .with_help_message("Enter SQL query")
-            .with_predefined_text("SELECT * FROM user")
-            .with_file_extension("sql")
-            .prompt()?;
+        loop {
+            let query = Editor::new("SQL query")
+                .with_help_message("Enter SQL query")
+                .with_file_extension("sql")
+                .with_validator(|q: &str| {
+                    if q.is_empty() {
+                        Ok(Validation::Invalid("Empty query".into()))
+                    } else {
+                        Ok(Validation::Valid)
+                    }
+                })
+                .prompt();
 
-        QueryPlanner::execute_query(&query);
-        Ok(())
+            if let Err(e) = query {
+                eprintln!("{:?}", e);
+                continue;
+            }
+
+            if let Err(e) = QueryPlanner::execute_query(&query.unwrap()) {
+                eprintln!("{:?}", e);
+            }
+        }
     }
 
     fn execute_query(raw_query: &str) -> Result<(), QueryPlannerError> {
         let query = QueryParser::parse(raw_query)?;
+
+        println!("[Query] {:?}", query);
+
+        match query {
+            Query::Database { name, action } => match action {
+                DatabaseAction::Create => {}
+                DatabaseAction::Drop => {}
+                DatabaseAction::Use => {}
+            },
+            Query::Table { name, query } => match query {
+                TableQuery::Create { cols } => {}
+                TableQuery::DropTable => {}
+                TableQuery::Truncate => {}
+                TableQuery::DropCol(col) => {}
+                TableQuery::AlterCol { col_name, datatype } => {}
+                TableQuery::AddCol { col_name, datatype } => {}
+                TableQuery::Select { cols, condition } => {}
+                TableQuery::Insert { cols, values } => {}
+                TableQuery::Delete { condition } => {}
+            },
+        }
 
         Ok(())
     }
